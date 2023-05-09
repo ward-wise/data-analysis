f8835ccaff68c0ac0a80b3e0ddb1dc7af5392eb1
Sean MacMullan <sean.macmullan@gmail.com>
2023-05-08 19:44:44 -0500

    Make address geocoding fuzzy. Geocoding will use nearby address coords if no exact match is found.

--- src/chicago_participatory_urbanism/geocoder.py
+++ src/chicago_participatory_urbanism/geocoder.py
@@ -15,15 +15,19 @@ def get_street_address_coordinates_from_full_name(address: str):
     Returns:
     - (float, float): A tuple containing the longitude and latitude.
     """
-    result = df[df['CMPADDABRV'] == address]
-    longitude = result["Long"][0]
-    latitude = result["Lat"][0]
+    result = df[df['CMPADDABRV'] == address.upper()]
+
+    try:
+        longitude = result["Long"].iloc[0]
+        latitude = result["Lat"].iloc[0]
+    except:
+        (longitude, latitude) = (0,0)
     
     return (longitude, latitude)
 
 
 
-def get_street_address_coordinates(address_number: int, direction_abbr: str, street_name: str, street_name_post_type_abbr: str):
+def get_street_address_coordinates(address_number: int, direction_abbr: str, street_name: str, street_name_post_type_abbr: str, fuzziness: int = 10):
     """
     Return the GPS coordinates of a street address in Chicago.
 
@@ -32,17 +36,35 @@ def get_street_address_coordinates(address_number: int, direction_abbr: str, str
     - direction_abbrev (str): An abbreviated cardinal direction. Ex: "W"
     - street_name (str): A street name in Chicago. Ex: "BELMONT"
     - street_name_post_type_abbr (str): An abbreviated street type. Ex: "AVE"
+    - fuzziness (int): The number of addresses +/- the desired address number when searching for coordinates. The function will always return the closest address' coordinates.
         
 
     Returns:
     - (float, float): A tuple containing the longitude and latitude.
     """
-    result = df[(df['Add_Number'] == address_number) &
-                (df['LSt_PreDir'] == direction_abbr) &
-                (df['St_Name'] == street_name) &
-                (df['LSt_Type'] == street_name_post_type_abbr)]
-    longitude = result["Long"][0]
-    latitude = result["Lat"][0]
+    results = df[(address_number - fuzziness <= df['Add_Number']) &
+                (df['Add_Number'] <= address_number + fuzziness) &
+                (df['LSt_PreDir'] == direction_abbr.upper()) &
+                (df['St_Name'] == street_name.upper()) &
+                (df['LSt_Type'] == street_name_post_type_abbr.upper())]
+    
+    # print(results[['Add_Number', 'St_Name','Long','Lat']])
+
+    exact_address = results[results['Add_Number'] == address_number]
+
+    try:
+        if not exact_address.empty:
+            # use exact address
+            longitude = exact_address["Long"].iloc[0]
+            latitude = exact_address["Lat"].iloc[0]
+        else:
+            # find closest address
+            results['difference'] = abs(results['Add_Number'] - address_number)
+            closest_index = results['difference'].idxmin()
+            longitude = results.loc[closest_index, "Long"]
+            latitude = results.loc[closest_index, "Lat"]
+    except:
+        (longitude, latitude) = (0, 0)
     
     return (longitude, latitude)
 
