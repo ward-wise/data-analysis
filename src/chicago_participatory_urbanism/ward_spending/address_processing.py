4daa5782932d59eec7b496773c01fdd70cd65a0a
Sean MacMullan <sean.macmullan@gmail.com>
2023-05-16 19:08:04 -0500

    Change segment geocoding to use intersections

--- src/chicago_participatory_urbanism/ward_spending/address_processing.py
+++ src/chicago_participatory_urbanism/ward_spending/address_processing.py
@@ -25,7 +25,7 @@ location_patterns = {
     # Pattern for format: N WOOD ST & W AUGUSTA BLVD & W CORTEZ ST & N HERMITAGE AVE
     LocationFormat.ALLEY: r"^[NWES]\s+\w+\s+\w+\s*&\s*[NWES]\s+\w+\s+\w+\s*&\s*[NWES]\s+\w+\s+\w+\s*&\s*[NWES]\s+\w+\s+\w+$",
     # Pattern for format: ON N LEAVITT ST FROM W DIVISION ST (1200 N) TO W NORTH AVE (1600 N)
-    LocationFormat.STREET_SEGMENT_INTERSECTIONS: r"^ON\s+([NWES]\s+\w+\s+\w+)\s+FROM\s+[NWES]\s+\w+\s+\w+\s+\((\d+)\s+[NWES]\)\s+TO\s+[NWES]\s+\w+\s+\w+\s+\((\d+)\s+[NWES]\)$",
+    LocationFormat.STREET_SEGMENT_INTERSECTIONS: r"^ON\s+[NWES]\s+(\w+)\s+\w+\s+FROM\s+[NWES]\s+(\w+)\s+\w+\s+\(\d+\s+[NWES]\)\s+TO\s+[NWES]\s+(\w+)\s+\w+\s+\(\d+\s+[NWES]\)$",
     # Pattern for format: ON W 52ND PL FROM 322 W TO S PRINCETON AVE (300 W)
     LocationFormat.STREET_SEGMENT_ADDRESS_INTERSECTION: r"^ON\s+[NWES]\s+\w+\s+\w+\s+FROM\s+\d+\s+[NWES]\s+TO\s+[NWES]\s+\w+\s+\w+\s+\(\d+\s+[NWES]\)$",
     # Pattern for format: ON W 52ND PL FROM S PRINCETON AVE (300 W) TO 322 W
@@ -45,7 +45,7 @@ def get_location_format(location):
 def extract_address_range(street_segment_intersections):
     """For the STREET_SEGMENT_INTERSECTIONS format, return a tuple with the address of the first and second intersection"""
     # Format: ON N LEAVITT ST FROM W DIVISION ST (1200 N) TO W NORTH AVE (1600 N)
-    pattern = location_patterns[LocationFormat.STREET_SEGMENT_INTERSECTIONS]
+    pattern = r"^ON\s+([NWES]\s+\w+\s+\w+)\s+FROM\s+[NWES]\s+\w+\s+\w+\s+\((\d+)\s+[NWES]\)\s+TO\s+[NWES]\s+\w+\s+\w+\s+\((\d+)\s+[NWES]\)$"
     # Check if the address matches the pattern
     match = re.match(pattern, street_segment_intersections)
     if match:
@@ -59,6 +59,21 @@ def extract_address_range(street_segment_intersections):
     else:
         return None, None
 
+
+def extract_street_names(street_segment_intersections):
+    """For the STREET_SEGMENT_INTERSECTIONS format, return a tuple with the primary street and two cross streets."""
+    # Format: ON N LEAVITT ST FROM W DIVISION ST (1200 N) TO W NORTH AVE (1600 N)
+    pattern = location_patterns[LocationFormat.STREET_SEGMENT_INTERSECTIONS]
+    # Check if the address matches the pattern
+    match = re.match(pattern, street_segment_intersections)
+    if match:
+        primary_street = match.group(1)
+        cross_street1 = match.group(2)
+        cross_street2 = match.group(3)
+        return primary_street, cross_street1, cross_street2
+    else:
+        return None, None, None
+    
 
 def get_location_text_format(text):
     """Return a string of detected formats. Use this function to debug address format matching en masse."""
@@ -90,52 +105,55 @@ def process_location_text(text):
 
 def get_geometry_from_location(location):
     format = get_location_format(location)
-    match format:
-        case LocationFormat.STREET_ADDRESS:
-            return get_geometry_from_street_address(location)
+    try:
+        match format:
+            case LocationFormat.STREET_ADDRESS:
+                return get_geometry_from_street_address(location)
 
-        case LocationFormat.STREET_ADDRESS_RANGE:
-            match = re.match(location_patterns[LocationFormat.STREET_ADDRESS_RANGE], location)
-            number1 = match.group(1)
-            number2 = match.group(2)
-            street = match.group(3)
+            case LocationFormat.STREET_ADDRESS_RANGE:
+                match = re.match(location_patterns[LocationFormat.STREET_ADDRESS_RANGE], location)
+                number1 = match.group(1)
+                number2 = match.group(2)
+                street = match.group(3)
 
-            address1 = f"{number1} {street}"
-            address2 = f"{number2} {street}"
-            point1 = get_geometry_from_street_address(address1)
-            point2 = get_geometry_from_street_address(address2)
-            street_segment = LineString([point1, point2])
-            return street_segment
+                address1 = f"{number1} {street}"
+                address2 = f"{number2} {street}"
+                point1 = get_geometry_from_street_address(address1)
+                point2 = get_geometry_from_street_address(address2)
+                street_segment = LineString([point1, point2])
+                return street_segment
 
-        case LocationFormat.INTERSECTION:
-            match = re.match(location_patterns[LocationFormat.INTERSECTION], location)
-            street1 = match.group(1)
-            street2 = match.group(2)
-            intersection = geocoder.get_intersection_coordinates(street1, street2)
-            return intersection
+            case LocationFormat.INTERSECTION:
+                match = re.match(location_patterns[LocationFormat.INTERSECTION], location)
+                street1 = match.group(1)
+                street2 = match.group(2)
+                intersection = geocoder.get_intersection_coordinates(street1, street2)
+                return intersection
 
-        case LocationFormat.STREET_SEGMENT_INTERSECTIONS:
-            # TO DO change to use intersection function instead of street address
-            (street_address1, street_address2) = extract_address_range(location)
-            point1 = get_geometry_from_street_address(street_address1)
-            point2 = get_geometry_from_street_address(street_address2)
-            street_segment = LineString([point1, point2])
-            return street_segment
+            case LocationFormat.STREET_SEGMENT_INTERSECTIONS:
+                (primary_street, cross_street1, cross_street2) = extract_street_names(location)
+                point1 = geocoder.get_intersection_coordinates(primary_street, cross_street1)
+                point2 = geocoder.get_intersection_coordinates(primary_street, cross_street2)
+                street_segment = LineString([point1, point2])
+                return street_segment
 
-        case LocationFormat.STREET_SEGMENT_ADDRESS_INTERSECTION:
-            #handle_street_segment_address_intersection()
-            return None
+            case LocationFormat.STREET_SEGMENT_ADDRESS_INTERSECTION:
+                #handle_street_segment_address_intersection()
+                return None
 
-        case LocationFormat.STREET_SEGMENT_INTERSECTION_ADDRESS:
-            #handle_street_segment_intersection_address()
-            return None
+            case LocationFormat.STREET_SEGMENT_INTERSECTION_ADDRESS:
+                #handle_street_segment_intersection_address()
+                return None
 
-        case LocationFormat.ALLEY:
-            #handle_alley()
-            return None
+            case LocationFormat.ALLEY:
+                #handle_alley()
+                return None
 
-        case _ :
-            return None
+            case _ :
+                return None
+            
+    except:
+        return None
 
 
 def get_geometry_from_street_address(street_address):
