3c6cd6dc7429e01750fefee8c716da94655ba3e7
Sean MacMullan <sean.macmullan@gmail.com>
2023-05-17 21:08:31 -0500

    Add support for multi-word streets. Refactor location regex patterns. Refactor address geocoding into its own module. Add alley geocoding.

---
+++ src/chicago_participatory_urbanism/ward_spending/address_format_processing.py
@@ -0,0 +1,89 @@
+import re
+from enum import auto, Enum
+
+
+class LocationFormat(Enum):
+    STREET_ADDRESS = auto()
+    STREET_ADDRESS_RANGE = auto()
+    INTERSECTION = auto()
+    STREET_SEGMENT_INTERSECTIONS = auto()
+    STREET_SEGMENT_ADDRESS_INTERSECTION = auto()
+    STREET_SEGMENT_INTERSECTION_ADDRESS = auto()
+    ALLEY = auto()
+
+
+street_suffixes = "(?:AVE|BLVD|CRES|CT|DR|ER|EXPY|HWY|LN|PKWY|PL|PLZ|RD|RL|ROW|SQ|SR|ST|TER|TOLL|WAY|XR)"
+street_pattern = rf"[NWES]\s(.*)\s{street_suffixes}(?:|\s+[NWES])"
+
+# TO DO: modify regex to work with "S AVENUE" and "N BROADWAY", "N LINCOLN PARK W", "W MIDWAY PARK", "W FULTON MARKET"
+location_patterns = {
+    # Pattern for format: 1640 N MAPLEWOOD AVE
+    LocationFormat.STREET_ADDRESS: rf"^\d+\s+{street_pattern}$",
+    # Pattern for format: 434-442 E 46TH PL
+    LocationFormat.STREET_ADDRESS_RANGE: rf"^(\d+)-(\d+)\s+{street_pattern}$",
+    # Pattern for format: N ASHLAND AVE & W CHESTNUT ST
+    LocationFormat.INTERSECTION: rf"^{street_pattern}\s*&\s*{street_pattern}$",
+    # Pattern for format: N WOOD ST & W AUGUSTA BLVD & W CORTEZ ST & N HERMITAGE AVE
+    LocationFormat.ALLEY: rf"^{street_pattern}\s*&\s*{street_pattern}\s*&\s*{street_pattern}\s*&\s*{street_pattern}$",
+    # Pattern for format: ON N LEAVITT ST FROM W DIVISION ST (1200 N) TO W NORTH AVE (1600 N)
+    LocationFormat.STREET_SEGMENT_INTERSECTIONS: rf"^ON\s+{street_pattern}\s+FROM\s+{street_pattern}\s*\(\d+\s+[NWES]\)\s*TO\s+{street_pattern}\s*\(\d+\s+[NWES]\)$",
+    # Pattern for format: ON W 52ND PL FROM 322 W TO S PRINCETON AVE (300 W)
+    LocationFormat.STREET_SEGMENT_ADDRESS_INTERSECTION: rf"^ON\s+{street_pattern}\s+FROM\s+\d+\s+[NWES]\s+TO\s+{street_pattern}\s*\(\d+\s+[NWES]\)$",
+    # Pattern for format: ON W 52ND PL FROM S PRINCETON AVE (300 W) TO 322 W
+    LocationFormat.STREET_SEGMENT_INTERSECTION_ADDRESS: rf"^ON\s+{street_pattern}\s+FROM\s+{street_pattern}\s*\(\d+\s+[NWES]\)\s+TO\s+\d+\s+[NWES]$",
+}
+
+
+def get_location_format(location):
+    """Detect and return the address format."""
+    for format, pattern in location_patterns.items():
+        if re.match(pattern, location.strip()):
+            return format
+
+
+    print(location)
+    return None
+
+
+def extract_address_range(street_segment_intersections):
+    """For the STREET_SEGMENT_INTERSECTIONS format, return a tuple with the address of the first and second intersection"""
+    # Format: ON N LEAVITT ST FROM W DIVISION ST (1200 N) TO W NORTH AVE (1600 N)
+    pattern = r"^ON\s+([NWES]\s+\w+\s+\w+)\s+FROM\s+[NWES]\s+\w+\s+\w+\s+\((\d+)\s+[NWES]\)\s+TO\s+[NWES]\s+\w+\s+\w+\s+\((\d+)\s+[NWES]\)$"
+    # Check if the address matches the pattern
+    match = re.match(pattern, street_segment_intersections)
+    if match:
+        street = match.group(1)
+        start_number = match.group(2)
+        end_number = match.group(3)
+        # Construct the two strings in the desired format
+        start_address = f"{start_number} {street}"
+        end_address = f"{end_number} {street}"
+        return start_address, end_address
+    else:
+        return None, None
+
+
+def extract_street_names(street_segment_intersections):
+    """For the STREET_SEGMENT_INTERSECTIONS format, return a tuple with the primary street and two cross streets."""
+    # Format: ON N LEAVITT ST FROM W DIVISION ST (1200 N) TO W NORTH AVE (1600 N)
+    pattern = location_patterns[LocationFormat.STREET_SEGMENT_INTERSECTIONS]
+    # Check if the address matches the pattern
+    match = re.match(pattern, street_segment_intersections)
+    if match:
+        primary_street = match.group(1)
+        cross_street1 = match.group(2)
+        cross_street2 = match.group(3)
+        return primary_street, cross_street1, cross_street2
+    else:
+        return None, None, None
+    
+
+def get_location_text_format(text):
+    """Return a string of detected formats. Use this function to debug address format matching en masse."""
+
+    locations = text.split(";")
+    format = ""
+    for location in locations:
+        format += str(get_location_format(location)) + ";"
+
+    return format
---
+++ src/chicago_participatory_urbanism/ward_spending/address_geocoding.py
@@ -0,0 +1,99 @@
+from shapely.geometry import Point, MultiPoint, LineString, Polygon
+import re
+import chicago_participatory_urbanism.geocoder as geocoder
+import chicago_participatory_urbanism.ward_spending.address_format_processing as afp
+
+
+def process_location_text(text):
+    """Take the location text from the ward spending data and return a geometry matching the GPS coordinates."""
+
+    locations = text.split(";")
+
+    geometry = None
+    for location in locations:
+        location_geometry = get_geometry_from_location(location)
+        # assign if geometry is empty, otherwise add to existing geometry
+        if geometry is None:
+            geometry = location_geometry
+        else:
+            geometry = geometry.union(location_geometry)
+
+    return geometry
+
+
+
+def get_geometry_from_location(location):
+    format = afp.get_location_format(location)
+    try:
+        match format:
+            case afp.LocationFormat.STREET_ADDRESS:
+                return get_geometry_from_street_address(location)
+
+            case afp.LocationFormat.STREET_ADDRESS_RANGE:
+                match = re.match(afp.location_patterns[afp.LocationFormat.STREET_ADDRESS_RANGE], location)
+                number1 = match.group(1)
+                number2 = match.group(2)
+                street = match.group(3)
+
+                address1 = f"{number1} {street}"
+                address2 = f"{number2} {street}"
+                point1 = get_geometry_from_street_address(address1)
+                point2 = get_geometry_from_street_address(address2)
+                street_segment = LineString([point1, point2])
+                return street_segment
+
+            case afp.afp.LocationFormat.INTERSECTION:
+                match = re.match(afp.location_patterns[afp.LocationFormat.INTERSECTION], location)
+                street1 = match.group(1)
+                street2 = match.group(2)
+                intersection = geocoder.get_intersection_coordinates(street1, street2)
+                return intersection
+
+            case afp.LocationFormat.STREET_SEGMENT_INTERSECTIONS:
+                (primary_street, cross_street1, cross_street2) = afp.extract_street_names(location)
+                point1 = geocoder.get_intersection_coordinates(primary_street, cross_street1)
+                point2 = geocoder.get_intersection_coordinates(primary_street, cross_street2)
+                street_segment = LineString([point1, point2])
+                return street_segment
+
+            case afp.LocationFormat.STREET_SEGMENT_ADDRESS_INTERSECTION:
+                #handle_street_segment_address_intersection()
+                return None
+
+            case afp.LocationFormat.STREET_SEGMENT_INTERSECTION_ADDRESS:
+                #handle_street_segment_intersection_address()
+                return None
+
+            case afp.LocationFormat.ALLEY:
+                match = re.match(afp.location_patterns[afp.LocationFormat.ALLEY], location)
+                street1 = match.group(1)
+                street2 = match.group(2)
+                street3 = match.group(3)
+                street4 = match.group(4)
+
+                point1 = geocoder.get_intersection_coordinates(street1, street2)
+                point2 = geocoder.get_intersection_coordinates(street2, street3)
+                point3 = geocoder.get_intersection_coordinates(street3, street4)
+                point4 = geocoder.get_intersection_coordinates(street4, street1)
+                alley_bounding_box = box = Polygon([point1, point2, point3, point4])
+                return alley_bounding_box
+
+            case _ :
+                print(f"Location text: {location}")
+                print(f"No format match found.\n")
+                return None
+            
+    except Exception as e:
+        print(f"Location text: {location}")
+        print(f"An error occurred: {str(e)}\n")
+        return None
+
+
+def get_geometry_from_street_address(street_address):
+    address_parts = street_address.strip().split(" ")
+    number = int(address_parts[0])
+    direction = address_parts[1]
+    name = " ".join(address_parts[2:-1]) #capture multi-word names
+    street_type = address_parts [-1]
+    geometry = geocoder.get_street_address_coordinates(number, direction, name, street_type, 20)
+    return geometry
