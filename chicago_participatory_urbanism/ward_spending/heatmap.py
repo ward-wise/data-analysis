60641c6eb7f7f4a3110e741cbf7eb0e7cf09d87f
Sean MacMullan <sean.macmullan@gmail.com>
2023-07-27 22:16:47 -0400

    Add multipoint to heatmap points function

--- chicago_participatory_urbanism/ward_spending/heatmap.py
+++ chicago_participatory_urbanism/ward_spending/heatmap.py
@@ -14,15 +14,17 @@ def export_to_heatmap_csv(geojson_data, output_csv_path):
 
         for feature in geojson_data["features"]:
             try:
-                feature_type = feature['geometry']['type']
+                geometry = feature['geometry']
+                feature_type = geometry['type']
                 properties = feature['properties']
+                cost = properties["cost"]
+                properties.pop("cost") # remove cost from properties
+
                 if feature_type == 'Point':
-                    coordinates = extract_point_coordinates(feature)
-                    row_data = {'latitude': coordinates[1], 'longitude': coordinates[0]}
-                    row_data.update(properties)
-                    writer.writerow(row_data)
-                # elif feature_type == 'MultiPoint':
-                    # divide cost equally among points
+                    points = [geometry['coordinates']]
+                    cost_per_point = cost
+                elif feature_type == 'MultiPoint':
+                    (points, cost_per_point) = split_cost_for_multipoint(geometry, cost)
                 # elif feature_type == 'LineString':
                     # generate a line of points with costs equally divided
                 # elif feature_type == 'MultiLineString':
@@ -34,14 +36,33 @@ def export_to_heatmap_csv(geojson_data, output_csv_path):
                 else:
                     # Note: MultiPolygons not present in original dataset
                     raise ValueError(f"Unsupported geometry type {feature_type} for {properties}")
+                
+                for coordinates in points:
+                    row_data = {'latitude': coordinates[1], 'longitude': coordinates[0]}
+                    row_data.update(properties)
+                    row_data.update( {'cost': cost_per_point})
+                    writer.writerow(row_data)
             except Exception as e:
                 print(e)
 
-def extract_point_coordinates(feature):
-    geometry = feature['geometry']
-    if geometry['type'] == 'Point':
-        return geometry['coordinates']
-    return None
+
+def split_cost_for_multipoint(geometry, combined_cost):
+    if geometry['type'] != 'MultiPoint':
+        raise ValueError("Invalid MultiPoint feature.")
+
+    combined_cost = float(combined_cost)
+    points = geometry['coordinates']
+
+    total_points = len(geometry['coordinates'])
+    if total_points == 0:
+        raise ValueError("MultiPoint feature contains no points.")
+
+    if combined_cost < 0:
+        raise ValueError("Cost per point should be a positive value.")
+
+    cost_per_point = combined_cost / total_points
+    return (points, cost_per_point)
+
 
 if __name__ == "__main__":
 
