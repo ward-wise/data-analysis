aa5ae0234edf7be0ebd94b6b031d1e7d111b7d1b
Sean MacMullan <sean.macmullan@gmail.com>
2023-07-29 00:25:51 -0400

    Add point interpolation for other geometry types

--- chicago_participatory_urbanism/ward_spending/heatmap.py
+++ chicago_participatory_urbanism/ward_spending/heatmap.py
@@ -1,7 +1,9 @@
 import json
 import csv
+import random
 import math
 from math import radians, sin, cos, sqrt, atan2
+from shapely.geometry import Point, Polygon, shape
 
 # determins how many points to split lines and polygons into
 POINT_RESOLUTION_DISTANCE = 0.010  # km
@@ -23,30 +25,18 @@ def export_to_heatmap_csv(geojson_data, output_csv_path):
         for feature in geojson_data["features"]:
             try:
                 geometry = feature['geometry']
-                feature_type = geometry['type']
                 properties = feature['properties']
                 combined_cost = properties["cost"]
                 properties.pop("cost")  # remove cost from properties
 
-                if feature_type == 'Point':
-                    points = [geometry['coordinates']]
-                elif feature_type == 'MultiPoint':
-                    points = geometry['coordinates']
-                elif feature_type == 'LineString':
-                    # generate a line of points with costs equally divided
-                    points = interpolate_points_from_line_string(geometry)
-                # elif feature_type == 'MultiLineString':
-                    # split costs across linestrings based on length
-                # elif feature_type == 'Polygon':
-                    # generate a cloud of points with costs equally divided
-                # elif feature_type == 'GeometryCollection':
+                if geometry == None:
+                    continue # TODO remove after testing
+                    raise ValueError("Feature is missing geometry.")
 
-                else:
-                    # Note: MultiPolygons not present in original dataset
-                    raise ValueError(f"Unsupported geometry type {feature_type}.")
+                # generate heatmap points and calculate cost per point
+                points = interpolate_points_from_geometry(geometry)
+                cost_per_point = float(combined_cost) / len(points)
 
-                combined_cost = float(combined_cost)
-                cost_per_point = combined_cost / len(points)
                 for coordinates in points:
                     row_data = {'latitude': coordinates[1], 'longitude': coordinates[0]}
                     row_data.update(properties)
@@ -54,6 +44,34 @@ def export_to_heatmap_csv(geojson_data, output_csv_path):
                     writer.writerow(row_data)
             except Exception as e:
                 print(f"{e} {properties}")
+
+
+# point interpolation functions
+
+def interpolate_points_from_geometry(geometry):
+    
+    feature_type = geometry['type']
+
+    if feature_type == 'Point':
+        points = [geometry['coordinates']]
+    elif feature_type == 'MultiPoint':
+        points = geometry['coordinates']
+    elif feature_type == 'LineString':
+        points = interpolate_points_from_line_string(geometry)
+    elif feature_type == 'MultiLineString':
+        points = interpolate_points_from_multi_line_string(geometry)
+    elif feature_type == 'Polygon':
+        points = interpolate_points_from_polygon(geometry)
+    elif feature_type == 'GeometryCollection':
+        points = []
+        for inner_geometry in geometry['geometries']:
+            #recursive call
+            points += interpolate_points_from_geometry(inner_geometry)
+    else:
+        # Note: MultiPolygons not present in original dataset
+        raise ValueError(f"Unsupported geometry type {feature_type}.")
+        
+    return points
 
 
 def interpolate_points_from_line_string(geometry):
@@ -79,6 +97,27 @@ def interpolate_points_from_line_string(geometry):
 
     return points
 
+
+def interpolate_points_from_multi_line_string(geometry):
+    points = []
+    for line_coordinates in geometry['coordinates']:
+        # create line feature to maintain function compatability
+        line_feature = {"type": "LineString", "coordinates":line_coordinates}
+
+        points += interpolate_points_from_line_string(line_feature)
+
+    return points
+
+
+def interpolate_points_from_polygon(geometry):
+    '''Return centroid of polygon (temporary implementation)'''
+    # TODO return point cloud
+    polygon = shape(geometry)
+    centroid = polygon.centroid
+    points = [[centroid.x, centroid.y]]
+    return points
+
+# helper functions
 
 def haversine_distance(coord1, coord2):
     # Approximate radius of Earth in km
@@ -127,9 +166,102 @@ def linear_interpolation(start_point, end_point, num_points):
         ratio = i / (num_points - 1)
         x = x0 + (x1 - x0) * ratio
         y = y0 + (y1 - y0) * ratio
-        interpolated_points.append((x, y))
+        interpolated_points.append([x, y])
 
     return interpolated_points
+
+def linear_interpolation2(points, num_points):
+    """ Returns an array of points through linear interpolation between the given points.
+
+    num_points = total number of interpolated points to return in the array (including input points).
+    """
+    if num_points < len(points):
+        raise ValueError("Number of points should be greater than or equal to the number of input points.")
+
+    if len(points) < 2:
+        raise ValueError("At least 2 points are required for interpolation.")
+
+    interpolated_points = []
+    step = (num_points - 1) / (len(points) - 1)
+
+    for i in range(len(points) - 1):
+        x0, y0 = points[i]
+        x1, y1 = points[i + 1]
+
+        for j in range(num_points):
+            ratio = j * step
+            x = x0 + (x1 - x0) * ratio
+            y = y0 + (y1 - y0) * ratio
+            interpolated_points.append((x, y))
+
+    return interpolated_points
+
+def linear_interpolation_by_distance(distance_along_line, data_points):
+    total_distance = 0.0
+    for i in range(len(data_points) - 1):
+        x1, y1 = data_points[i]
+        x2, y2 = data_points[i + 1]
+        segment_distance = calculate_distance((x1, y1), (x2, y2))
+        total_distance += segment_distance
+        if total_distance >= distance_along_line:
+            remaining_distance = total_distance - distance_along_line
+            x = x2 - remaining_distance * (x2 - x1) / segment_distance
+            y = y2 - remaining_distance * (y2 - y1) / segment_distance
+            return (x,y)
+
+    # If distance_along_line is beyond the last point, return the last point's coordinates.
+    return data_points[-1]
+
+
+def generate_points_in_polygon(polygon_points, max_distance):
+    polygon = Polygon(polygon_points)
+    min_distance = max_distance / (2**0.5)  # Minimum distance between points
+
+    def is_valid_point(point):
+        return polygon.contains(Point(point)) and all(
+            Point(p).distance(Point(point)) >= min_distance for p in sample_points
+        )
+
+    sample_points = []
+    active_points = []
+
+    def add_point(point):
+        active_points.append(point)
+        sample_points.append(point)
+
+    # Choose a random point inside the polygon to start
+    start_x = random.uniform(min(polygon_points, key=lambda p: p[0])[0],
+                             max(polygon_points, key=lambda p: p[0])[0])
+    start_y = random.uniform(min(polygon_points, key=lambda p: p[1])[1],
+                             max(polygon_points, key=lambda p: p[1])[1])
+
+    add_point((start_x, start_y))
+
+    while active_points:
+        random_index = random.randint(0, len(active_points) - 1)
+        active_point = active_points[random_index]
+
+        found_new_point = False
+        for _ in range(30):  # Try a maximum of 30 candidate points for each active point
+            angle = random.uniform(0, 2 * 3.141592653589793)  # Random angle in radians
+            distance = random.uniform(min_distance, 2 * min_distance)
+            new_x = active_point[0] + distance * math.cos(angle)
+            new_y = active_point[1] + distance * math.sin(angle)
+            new_point = (new_x, new_y)
+
+            if is_valid_point(new_point):
+                add_point(new_point)
+                found_new_point = True
+                break
+
+        if not found_new_point:
+            active_points.pop(random_index)
+
+    return sample_points
+
+
+
+
 
 
 if __name__ == "__main__":
