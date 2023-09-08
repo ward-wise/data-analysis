79242412e4160c9b94841406b218096516b3b587
kollerbud <38870258+kollerbud@users.noreply.github.com>
2023-09-08 19:26:15 +0000

    api wrapper working

--- chicago_participatory_urbanism/ward_spending/post_processor.py
+++ chicago_participatory_urbanism/ward_spending/post_processor.py
@@ -1,24 +1,71 @@
 import pandas as pd
-from ward_spending.categorization import STANDARD_CATEGORY
+#from categorization import STANDARD_CATEGORY
+from location_geocoding import LocationGeocoder
+from geocoder_api import GeoCoderAPI
+import multiprocessing as mp
 
-def post_process_data(file_name: str, year: int):
+
+STANDARD_CATEGORY = {
+    "pedestrian": "Pedestrian Infrastructure",
+    "bump outs": "Pedestrian Infrastructure",
+    "bicycle": "Bicycle Infrastructure",
+    "bike": "Bicycle Infrastructure",
+    "neighborhood greenway": "Bicycle Infrastructure",
+    "light": "Lighting",
+    "street resurfacing": "Street Resurfacing",
+    "street speed hump replacement": "Street Resurfacing",
+    "curb & gutter": "Street Resurfacing",
+    "alley": "Alleys",
+    "miscellaneous cdot projects": "Misc. CDOT",
+    "mural": "Beautification",
+    "public art": "Beautification",
+    "tree planting": "Beautification",
+    "turn arrow": "Street Redesign",
+    "street speed hump menu": "Street Redesign",
+    "pavement markings": "Street Redesign",
+    "traffic circle": "Street Redesign",
+    "cul-de-sac": "Street Redesign",
+    "diagnol parking": "Street Redesign",
+    "sidewalk": "Sidewalk Repair",
+    "pod camera": "Police Cameras",
+    "park": "Parks",
+    "playground": "Parks",
+    "garden": "Parks",
+    "viaduct": "Viaducts",
+}
+
+def post_process_data(file_name: str):
     # load in data and mark year
     data = pd.read_csv(file_name, index_col=None)
-    data['year'] = year
-
-    ## clean up data
+    # data['year'] = year
     # remove year from items and add as new column
     data['item'] = data['item'].str.replace(r'\s\(\d+\)', '', regex=True)
     # convert texts to lower case
     data['item'] = data['item'].str.lower()
     # convert cost column to numeric
-    data['cost'] = data['cost'].str.replace('[\$,]', '', regex=True).astype(float)
+    #data['cost'] = data['cost'].str.replace('[\$,]', '', regex=True).astype(float)
     # add category
     #data["category"] = data["item"].apply(get_menu_category)
 
     # map to standard categories
-    data["category"] = data["item"].map(STANDARD_CATEGORY)
+    data["category"] = data["item"].replace(STANDARD_CATEGORY, regex=True)
     # remaining categories are "Misc."
     data['category'] = data['category'].fillna("Misc.")
 
     return data
+
+
+if __name__ == '__main__':
+    import time
+    start = time.time()
+    data = post_process_data(file_name='data/ward_spending/2019-2022 data.csv')
+    geo_coder_method = GeoCoderAPI()
+    geo_coder = LocationGeocoder(geocoder=geo_coder_method)
+    with mp.Pool(mp.cpu_count()) as pool:
+        data_geo_code = pool.map(geo_coder.process_location_text, data[:100]['location'])
+    # for row in data[:100].itertuples():
+    #     geo_coder.process_location_text(text=row[3])
+
+    end = time.time()
+    print(data_geo_code)
+    print(f'{end-start} seconds')
