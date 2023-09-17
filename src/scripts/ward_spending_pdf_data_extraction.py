c2816aec2a54594a0135ec6e02ff76336706165d
Sean MacMullan <Sean.MacMullan@ardaghgroup.com>
2023-09-17 17:20:53 -0500

    Add multiple file names to pdf extract script

--- src/scripts/ward_spending_pdf_data_extraction.py
+++ src/scripts/ward_spending_pdf_data_extraction.py
@@ -1,11 +1,16 @@
 import os
 from chicago_participatory_urbanism.ward_spending.extract_text_from_pdf import extract_pdf_data
 
-pdf_file_name = '2020 Menu Posting - 22-10-02.pdf'
-output_file_name = pdf_file_name[:-4] + '.csv'
+files = ['2019 Menu Posting - 22-10-02.pdf', 
+         '2020 Menu Posting - 22-10-02.pdf',
+         '2021 Menu Posting - 22-10-02.pdf',
+         '2022 Menu - 2-9-23.pdf']
 
-pdf_file_path = os.path.join(os.getcwd(), 'data', 'pdf', pdf_file_name)
-output_file_path = os.path.join(os.getcwd(), 'data', 'output', output_file_name)
+for pdf_file_name in files:
+    output_file_name = pdf_file_name[:-4] + '.csv'
+
+    pdf_file_path = os.path.join(os.getcwd(), 'data', 'pdf', pdf_file_name)
+    output_file_path = os.path.join(os.getcwd(), 'data', 'output', output_file_name)
 
 
-extract_pdf_data(pdf_file_path, output_file_path)
+    extract_pdf_data(pdf_file_path, output_file_path)
