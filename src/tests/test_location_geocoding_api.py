90c1cec02aad4cfe1267a8b2557274962bb071b3
Sean MacMullan <sean.macmullan@gmail.com>
2023-10-12 22:35:34 -0500

    Add vscode testing setup. Fix geocoding test file

--- src/tests/test_location_geocoding_api.py
+++ src/tests/test_location_geocoding_api.py
@@ -1,10 +1,18 @@
 import unittest
 from chicago_participatory_urbanism.geocoder_api import GeoCoderAPI
 from chicago_participatory_urbanism.ward_spending.location_geocoding import LocationGeocoder
-from test_cases import address_tests
+from cases import address_tests
 
-if __name__ == '__main__':
+class TestGeocoding(unittest.TestCase):
 
+    # TODO make generic and add more cases
+    def test_geocoder_API_initialization(self):
+        geo_coder = GeoCoderAPI()
+        self.assertIsInstance(geo_coder, GeoCoderAPI, "Object is not an instance of GeoCodeAPI")
+
+
+# TODO needs to be converted to work with unittest framework
+def run_geocoder_api():
     geo_coder = GeoCoderAPI()
     for test in address_tests():
         print('---'*30)
@@ -12,3 +20,6 @@ if __name__ == '__main__':
         print(LocationGeocoder(geocoder=GeoCoderAPI()).process_location_text(
                 text=test)
         )
+
+if __name__ == '__main__':
+    unittest.main()
