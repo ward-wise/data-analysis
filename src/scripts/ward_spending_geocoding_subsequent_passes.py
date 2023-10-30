4ca302e3c79792748c842be7f89d142786108d39
Sean MacMullan <sean.macmullan@gmail.com>
2023-10-29 23:29:22 -0500

    Add script for geocoding missing geometries. Fix invalid geometry data in geocoded data.

---
+++ src/scripts/ward_spending_geocoding_subsequent_passes.py
@@ -0,0 +1,25 @@
+"""Take an existing geojson file and geocode items with missing geometry data."""
+import geopandas as gpd
+from chicago_participatory_urbanism.ward_spending.location_geocoding import LocationGeocoder
+
+# file paths
+input_geojson_file = r'data\2019-2022 data_geocoded.geojson'
+output_geojson_file = r'data\output\2019-2022 data_geocoded_new.geojson'
+
+# setup geocoder
+from chicago_participatory_urbanism.geocoder_api import GeoCoderAPI
+geocoder = GeoCoderAPI()
+location_geocoder = LocationGeocoder(geocoder)
+
+
+# geocode entries with missing geometry data
+gdf = gpd.read_file(input_geojson_file)
+for index, row in gdf.iterrows():
+    if row['geometry'] is None:
+        location_text = row['location']
+        geometry = location_geocoder.process_location_text(location_text)
+        gdf.at[index, 'geometry'] = geometry
+
+
+gdf.to_file(output_geojson_file, driver='GeoJSON')
+print(f"Updated GeoJSON saved to {output_geojson_file}")
