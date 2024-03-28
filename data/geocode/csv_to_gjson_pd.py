47a626e9867ac747de1b9e07ab0d006bef3b3544
Surya Dutta <surya@suryadutta.com>
2024-03-28 18:23:20 -0500

    revert all changes to data

--- data/geocode/csv_to_gjson_pd.py
+++ data/geocode/csv_to_gjson_pd.py
@@ -11,6 +11,7 @@ gdf = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
 #Rename columns if necessary. These columns will show up as a "property" of each coordinate in the geoJSON file, so clear labeling will be helpful.
 gdf = gdf.rename(columns={'CMPADDABRV':'Address', 'Unnamed: 0':'ID'})
 #Drop redundant Latitude and Longitude columns if necessary. This massively reduces the file size.
-gdf = gdf.drop(columns=['Lat', 'Long'])
+gdf = gdf.drop(columns=['Lat', 'Long']) 
 
 gdf.to_file('geocode/address_points.geojson', driver="GeoJSON") #Specify file path for geoJSON output
+
