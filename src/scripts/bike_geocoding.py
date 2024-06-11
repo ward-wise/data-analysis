4a61e56983141b742d5d40aefd04d6ab90124c6f
Sean MacMullan <sean.macmullan@gmail.com>
2024-06-11 11:21:52 -0700

    Update bikeway geocoding to use API and geocoding structures. Update CDOT bikeway data for 2024.

--- src/scripts/bike_geocoding.py
+++ src/scripts/bike_geocoding.py
@@ -1,24 +1,27 @@
 import geopandas as gpd
 import os
 from shapely.geometry import LineString
+from src.chicago_participatory_urbanism.location_structures import Intersection, Street
+from src.chicago_participatory_urbanism.geocoder_api import GeoCoderAPI
+geocoder = GeoCoderAPI()
 
-from src import chicago_participatory_urbanism as geocoder
-
-
-def process_street_segment(primary_street, cross_street1, cross_street2):
+def process_street_segment(primary_street_name, cross_street1_name, cross_street2_name):
     try:
-        point1 = geocoder.get_intersection_coordinates(primary_street, cross_street1)
-        point2 = geocoder.get_intersection_coordinates(primary_street, cross_street2)
+        primary_street = Street(direction="", name=primary_street_name, street_type="")
+        cross_street1 = Street(direction="", name=cross_street1_name, street_type="")
+        cross_street2 = Street(direction="", name=cross_street2_name, street_type="")
+    
+        point1 = geocoder.get_intersection_coordinates(Intersection(primary_street, cross_street1))
+        point2 = geocoder.get_intersection_coordinates(Intersection(primary_street, cross_street2))
         street_segment = LineString([point1, point2])
         return street_segment
-    except Exception:
+    except Exception as e:
+        print(e)
         return None
 
 
 def generate_bikeway_installations_geocoding():
 
     data = gpd.read_file(os.path.join(os.getcwd(), 'data', 'CDOT Bikeway Installations.csv'))
-
-
     data["geometry"] = data.apply(lambda row: process_street_segment(row['Street'], row['From'], row['To']), axis=1)
     data.to_file(os.path.join(os.getcwd(), 'data', 'CDOT Bikeway Installations.geojson'), driver='GeoJSON')
